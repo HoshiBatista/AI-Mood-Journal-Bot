f30811e27d030c48bdc2c9a1b056009aae2efdc9
Roman Moroz <morozdef24@gmail.com>
2025-07-15 09:03:41 +0300

    create config.py

---
+++ config/config.py
@@ -0,0 +1,48 @@
+from typing import List
+
+from pydantic import SecretStr, field_validator
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    BOT_TOKEN: SecretStr
+
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: SecretStr
+    POSTGRES_DB: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+
+    MONGO_USER: str
+    MONGO_PASSWORD: SecretStr
+    MONGO_DB: str
+    MONGO_HOST: str
+    MONGO_PORT: int
+
+    PGADMIN_EMAIL: str = ""   
+    PGADMIN_PASSWORD: str = "" 
+    
+    model_config = SettingsConfigDict(
+        env_file=".env", 
+        extra="ignore",
+        env_prefix=""  
+    )
+    
+    @property
+    def postgres_url(self):
+        return (
+            f"postgresql+asyncpg://{self.POSTGRES_USER}:"
+            f"{self.POSTGRES_PASSWORD.get_secret_value()}@"
+            f"{self.POSTGRES_HOST}:"
+            f"{self.POSTGRES_PORT}/"
+            f"{self.POSTGRES_DB}"
+        )
+        
+    @property
+    def mongo_url(self):
+        return (
+            f"mongodb://{self.MONGO_USER}:{self.MONGO_PASSWORD.get_secret_value()}"
+            f"@{self.MONGO_HOST}:{self.MONGO_PORT}/"
+            f"{self.MONGO_DB}?authSource=admin"
+        )
+
+config = Settings()
