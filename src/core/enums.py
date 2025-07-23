e29d40da1c39673a30cc91d19b1aa52e99726a6b
Roman Moroz <morozdef24@gmail.com>
2025-07-23 21:53:28 +0300

    continue make enums

--- src/core/enums.py
+++ src/core/enums.py
@@ -4,3 +4,19 @@ from enum import Enum
 class UserRole(str, Enum):
     USER = "user"
     PSYCHOLOGIST = "psychologist"
+    
+class MoodLevel(int, Enum):
+    VERY_BAD = 1
+    BAD = 2
+    NEUTRAL = 3
+    GOOD = 4
+    EXCELLENT = 5
+    
+class TriggerType(str, Enum):
+    WORK = "work"
+    RELATIONSHIPS = "relationships"
+    HEALTH = "health"
+    FINANCE = "finance"
+    FAMILY = "family"
+    SOCIAL = "social"
+    SELF_CARE = "self_care"
